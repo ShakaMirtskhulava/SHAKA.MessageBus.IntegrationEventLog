fc43a0d0012c8a2998a222b2d48410ea8a91ab4b
Cherbaki Zondikize <samudcherbak2018@gmail.com>
2025-03-26 09:59:05 +0400

    Remove IUnitOfWorkInterface

--- IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MessageBus.IntegrationEventLog;
-
-public interface IUnitOfWork
-{
-    Task BeginTransaction(CancellationToken cancellationToken);
-    Task RollbackTransaction(CancellationToken cancellationToken);
-    Task CommitTransaction(CancellationToken cancellationToken);
-    Task<T> ExecuteOnDefaultStarategy<T>(Func<Task<T>> operation);
-}
